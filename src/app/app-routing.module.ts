3babebf885aa65c7730cf5fb7f3667536e76d492
rodelio30 <rodeliobdomingojr@gmail.com>
2022-02-07 22:12:06 +0800

    generate page pages/home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   }
 ];
 
